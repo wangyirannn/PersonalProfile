2bfe57a2078384adf6015ce6954d5316f70d609b
wangyirannn <1296870006@qq.com>
2016-10-11 19:15:19 +0800

    完成缓动动画----复合缓动功能

--- PersonalProfile/bin-debug/Main.js
+++ PersonalProfile/bin-debug/Main.js
@@ -33,8 +33,59 @@ var Main = (function (_super) {
         //当前页数
         this.pageNumCurrent = 0;
         this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+        //this.once( egret.Event.ADDED_TO_STAGE, this.onAddToStage, this );
     }
     var d = __define,c=Main,p=c.prototype;
+    p.imgLoadHandler = function (evt) {
+        var bmd = evt.currentTarget.data;
+        /// 设计几个位置便于运动  左上 右下 左下 右上
+        this._vcLocation = [
+            new egret.Point(bmd.width / 2, 100 + bmd.height / 2),
+            new egret.Point(this.stage.stageWidth - bmd.width / 2, this.stage.stageHeight - bmd.height / 2),
+            new egret.Point(bmd.width / 2, this.stage.stageHeight - bmd.height / 2),
+            new egret.Point(this.stage.stageWidth - bmd.width / 2, 100 + bmd.height / 2)
+        ];
+        this._rotCommon = 180 / Math.PI * Math.atan2(this._vcLocation[1].y - this._vcLocation[0].y, this._vcLocation[1].x - this._vcLocation[0].x);
+        this._bird = new egret.Bitmap(bmd);
+        this._bird.anchorOffsetX = bmd.width / 2;
+        this._bird.anchorOffsetY = bmd.height / 2;
+        this.addChild(this._bird);
+        /// 提示信息
+        this._txInfo = new egret.TextField;
+        this.addChild(this._txInfo);
+        this._txInfo.size = 28;
+        this._txInfo.x = 50;
+        this._txInfo.y = 50;
+        this._txInfo.width = this.stage.stageWidth - 100;
+        this._txInfo.textAlign = egret.HorizontalAlign.LEFT;
+        this._txInfo.textColor = 0x000000;
+        this._txInfo.type = egret.TextFieldType.DYNAMIC;
+        this._txInfo.lineSpacing = 6;
+        this._txInfo.multiline = true;
+        this._txInfo.text =
+            "这是由四个阶段的缓动动画组成的复合动画，并且往复循环（您不需要接触屏幕！）";
+        /// 开启显示
+        this._idxCurrLocation = -1;
+        this._bird.x = this._vcLocation[3].x;
+        this._bird.y = this._vcLocation[3].y;
+        this._bird.rotation = -90;
+        console.log(this._rotCommon);
+        this.launchTween();
+    };
+    p.launchTween = function () {
+        var _this = this;
+        /*** 本示例关键代码段开始 ***/
+        egret.Tween.get(this._bird, { loop: true })
+            .to({ x: this._vcLocation[0].x, y: this._vcLocation[0].y }, 500)
+            .call(function () { _this._bird.rotation = 180 - _this._rotCommon; }).wait(200)
+            .to({ x: this._vcLocation[1].x, y: this._vcLocation[1].y }, 500)
+            .call(function () { _this._bird.rotation = -90; }).wait(200)
+            .to({ x: this._vcLocation[2].x, y: this._vcLocation[2].y }, 500)
+            .call(function () { _this._bird.rotation = _this._rotCommon; }).wait(200)
+            .to({ x: this._vcLocation[3].x, y: this._vcLocation[3].y }, 500)
+            .call(function () { _this._bird.rotation = -90; }).wait(200);
+        /*** 本示例关键代码段结束 ***/
+    };
     p.onAddToStage = function (event) {
         //设置加载进度界面
         //Config to load process interface
@@ -44,6 +95,9 @@ var Main = (function (_super) {
         //initiate Resource loading library
         RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
         RES.loadConfig("resource/default.res.json", "resource/");
+        var imgLoader = new egret.ImageLoader;
+        imgLoader.once(egret.Event.COMPLETE, this.imgLoadHandler, this);
+        imgLoader.load("resource/cartoon-egret_00.png");
     };
     /**
      * 配置文件加载完成,开始预加载preload资源组。
@@ -242,6 +296,7 @@ var Main = (function (_super) {
         this.addChild(icon);
         icon.x = 26;
         icon.y = 33;
+        this.addChild(this._bird);
         //pageTurning(pageNumCurrent);
         //2016touch
         /*icon.touchEnabled = true;
--- PersonalProfile/src/Main.ts
+++ PersonalProfile/src/Main.ts
@@ -37,9 +37,80 @@ class Main extends egret.DisplayObjectContainer {
     //当前页数
     private pageNumCurrent: number = 0;
 
+    private _bird:egret.Bitmap;
+    private _txInfo:egret.TextField;
+    
+    private _vcLocation:Array<egret.Point>;
+    private _idxCurrLocation:number;
+    
+    private _rotCommon:number;
+
     public constructor() {
         super();
         this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+        //this.once( egret.Event.ADDED_TO_STAGE, this.onAddToStage, this );
+    }
+
+    private imgLoadHandler( evt:egret.Event ):void{
+        
+        var bmd:egret.BitmapData = evt.currentTarget.data;
+        
+        /// 设计几个位置便于运动  左上 右下 左下 右上
+        this._vcLocation = [
+            new egret.Point( bmd.width/2, 100 + bmd.height/2 )
+            ,new egret.Point( this.stage.stageWidth - bmd.width/2, this.stage.stageHeight - bmd.height/2 )
+            ,new egret.Point( bmd.width/2, this.stage.stageHeight - bmd.height/2 )
+            ,new egret.Point( this.stage.stageWidth - bmd.width/2, 100 + bmd.height/2 )
+        ];
+        
+        this._rotCommon = 180/Math.PI * Math.atan2( 
+            this._vcLocation[1].y - this._vcLocation[0].y, this._vcLocation[1].x - this._vcLocation[0].x );
+        
+        this._bird = new egret.Bitmap( bmd );
+        this._bird.anchorOffsetX = bmd.width/2;
+        this._bird.anchorOffsetY = bmd.height/2;
+        this.addChild( this._bird );
+
+        /// 提示信息
+        this._txInfo = new egret.TextField;
+        this.addChild( this._txInfo );
+
+        this._txInfo.size = 28;
+        this._txInfo.x = 50;
+        this._txInfo.y = 50;
+        this._txInfo.width = this.stage.stageWidth - 100;
+        this._txInfo.textAlign = egret.HorizontalAlign.LEFT;
+        this._txInfo.textColor = 0x000000;
+        this._txInfo.type = egret.TextFieldType.DYNAMIC;
+        this._txInfo.lineSpacing = 6;
+        this._txInfo.multiline = true;
+        
+        this._txInfo.text =
+                "这是由四个阶段的缓动动画组成的复合动画，并且往复循环（您不需要接触屏幕！）";
+        
+        /// 开启显示
+        this._idxCurrLocation = -1;
+        
+        this._bird.x = this._vcLocation[3].x;
+        this._bird.y = this._vcLocation[3].y;
+        this._bird.rotation = - 90;
+        console.log( this._rotCommon );
+
+        this.launchTween();
+    }
+
+        private launchTween(){
+        /*** 本示例关键代码段开始 ***/
+        egret.Tween.get( this._bird, { loop:true} )
+            .to( {x:this._vcLocation[0].x, y:this._vcLocation[0].y}, 500 )
+                .call( ()=>{ this._bird.rotation = 180 - this._rotCommon;  } ).wait( 200 )
+            .to( {x:this._vcLocation[1].x, y:this._vcLocation[1].y}, 500 )
+                .call( ()=>{ this._bird.rotation = - 90; } ).wait( 200 )
+            .to( {x:this._vcLocation[2].x, y:this._vcLocation[2].y}, 500 )
+                .call( ()=>{ this._bird.rotation = this._rotCommon; } ).wait( 200 )
+            .to( {x:this._vcLocation[3].x, y:this._vcLocation[3].y}, 500 )
+                .call( ()=>{ this._bird.rotation = - 90; } ).wait( 200 );
+        /*** 本示例关键代码段结束 ***/                                                                                                
     }
 
     private onAddToStage(event:egret.Event) {
@@ -52,6 +123,10 @@ class Main extends egret.DisplayObjectContainer {
         //initiate Resource loading library
         RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
         RES.loadConfig("resource/default.res.json", "resource/");
+
+        var imgLoader:egret.ImageLoader = new egret.ImageLoader;
+        imgLoader.once( egret.Event.COMPLETE, this.imgLoadHandler, this );
+        imgLoader.load( "resource/cartoon-egret_00.png" );
     }
 
     /**
@@ -261,6 +336,7 @@ class Main extends egret.DisplayObjectContainer {
         this.addChild(icon);
         icon.x = 26;
         icon.y = 33;
+        this.addChild( this._bird );
 
         //pageTurning(pageNumCurrent);
         //2016touch
